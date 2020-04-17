888d53b6f632c76e2d335a640fc32876563d62fb
Solomon APPIER-SIGN <braasig@gmail.com>
2020-04-17 20:13:02 +0000

    added game steps

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,17 @@
 #!/usr/bin/env ruby
 
-puts 'Hello World!'
+puts 'Hi! Welcome to play Tic Tac Toe'
+puts 'Player 1 enter your name'
+# get player_1's name
+puts 'Player 2 enter your name'
+# get player_2's name
+
+# display game board
+puts 'Player_1 choose your number'
+# get player_1's number
+puts 'Player_2 choose your number'
+# get player_2's number
+
+#repeat steps above until there's a winner or no more moves
+puts 'Game over'
+puts 'Winner is Player'
