b0147fd1ae47ec8087e73983524e2241b8b8c62f
aulbourn knowles <aulbourn@gmail.com>
2020-04-20 18:01:40 -0400

    updating player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,13 @@
 class Player
+  @@player = {}
+  @@player_count = 0
   attr_reader :name
   attr_writer :name
-  def initialize name
+  def initialize name 
     begin
       raise 'Not a vaild name, must be a string' unless name_is_valid?(name)
       @name = name
+      @@player_count += 1
     end
   end
 
@@ -15,7 +18,17 @@ class Player
        false
     end
   end
+
+  def assign_player_character
+    if name_is_valid? && player_count == 1
+      @@player[name] = 'X'
+    elsif name_is_valid? && player_count == 2
+      @@player[name] = 'O'
+    end
+  end
 end
+
+
 
 me = Player.new(12)
 print me
