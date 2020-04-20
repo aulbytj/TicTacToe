deeca5176f35d90c0ff8054c4ab831dce5b23276
Solomon APPIER-SIGN <braasig@gmail.com>
2020-04-20 17:25:03 +0000

    added cells

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,11 @@
 class Board
-  def initialize board
-    @board = [
-        [],
-        [],
-        []
+  attr_reader :cells
+
+  def initialize
+    @cells = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
     ]
   end
 end
