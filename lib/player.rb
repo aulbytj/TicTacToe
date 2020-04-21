1bd9c387ed1982da42cb552244965ac5f8ce5c15
Solomon APPIER-SIGN <braasig@gmail.com>
2020-04-21 20:03:13 +0000

    added validation for already selected name

--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,20 @@
 class Player
-  @@player = {}
+  @@players = []
   @@count = 0
+
   @character
+
   attr_reader :name
   attr_writer :name
   attr_reader :character
-  def initialize name
+  attr_reader :players
+
+  def initialize(name)
     begin
-      raise 'Not a vaild name, must be a string' unless name_is_valid?(name)
+      raise 'Not a valid name, must be a string' unless name_is_valid?(name)
+      raise 'Name already selected' if @@players.any?(name)
       @name = name
+      @@players << name
       @@count += 1
       if @@count == 1
         @character = 'X'
@@ -31,6 +37,7 @@ end
 
 me = Player.new("jay")
 you = Player.new('stacy')
+
 print me.character
 print you.character
 
