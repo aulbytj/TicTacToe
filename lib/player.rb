13dfb2d09e954d19409babd28ac65ad4265ed535
Solomon APPIER-SIGN <braasig@gmail.com>
2020-04-22 16:22:31 +0000

    removed @@players class variable

--- lib/player.rb
+++ lib/player.rb
@@ -1,18 +1,13 @@
 class Player
-  @@players = []
   attr_reader :name
   attr_reader :character
 
-  def initialize(name, character)
+  def initialize(name, character, players = [])
     raise 'Not a valid name, must be a string' unless name_is_valid?(name)
-    raise 'Name already selected' if @@players.any?(name)
+    raise 'Name already selected' if players.any?(name)
 
     @name = name
     @character = character
-  end
-
-  def self.players
-    @@players
   end
 
   def name_is_valid?(name)
