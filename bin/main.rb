05344ed28c9bd21e237462b9838393121bf99a1d
Solomon APPIER-SIGN <braasig@gmail.com>
2020-04-22 16:05:05 +0000

    fixed rubocop offenses

--- bin/main.rb
+++ bin/main.rb
@@ -3,12 +3,7 @@
 require_relative '../lib/board'
 require_relative '../lib/player'
 
-
 class Game
-  @players
-  @current_player
-  @board
-
   def initialize
     @players = []
     @current_player = 0
@@ -18,16 +13,16 @@ class Game
   end
 
   private
+
   def set_players
     while Player.players.length < 2
       puts "Player #{Player.players.length + 1} enter your name: "
       name = gets.chomp
-      Player.players << Player.new(name, get_character)
+      Player.players << Player.new(name, character)
     end
   end
 
-  private
-  def get_character
+  def character
     Player.players.empty? ? 'X' : 'O'
   end
 
@@ -37,24 +32,23 @@ class Game
 
   def start_session
     create_board
-    while @board.has_moves?
+    status = 'its a tie!'
+    while @board.moves?
       p @board.cells
-      p "select your number to play"
+      p 'select your number to play'
       number = gets.chomp.to_i
       outcome = @board.move(number, Player.players[@current_player].character)
-      if outcome === 1
-        p "#{Player.players[@current_player].name} won!"
+      if outcome == 1
+        status = "#{Player.players[@current_player].name} won!"
         break
-      elsif outcome === -1
-        p "cell #{number} is not available, try again"
-      elsif outcome === 0
-        p "its a tie!"
+      elsif outcome == -1
+        status = "cell #{number} is not available, try again"
       else
         @current_player = @current_player == 1 ? 0 : 1
       end
     end
+    p status
   end
 end
 
 Game.new
-
