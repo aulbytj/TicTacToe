002af3f9f9531bf68a47bd7bd33da87a95869865
Solomon APPIER-SIGN <braasig@gmail.com>
2020-04-22 23:45:40 +0000

    removed raise statements added capitalize to name removed validation

--- lib/player.rb
+++ lib/player.rb
@@ -2,19 +2,8 @@ class Player
   attr_reader :name
   attr_reader :character
 
-  def initialize(name, character, players = [])
-    raise 'Not a valid name, must be a string' unless name_is_valid?(name)
-    raise 'Name already selected' if players.any?(name)
-
-    @name = name
+  def initialize(name, character)
+    @name = name.capitalize
     @character = character
-  end
-
-  def name_is_valid?(name)
-    if name.is_a? String
-      true
-    else
-      false
-    end
   end
 end
