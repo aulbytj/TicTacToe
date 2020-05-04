ffa80c07b1ab69c91f8e5f99c70512acd80a0801
aulbourn knowles <aulbourn@gmail.com>
2020-05-04 18:37:35 -0400

    added test game_spec.rb began test cases

---
+++ bin/spec/game_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../../lib/game'
+require_relative '../../lib/player'
+
+describe Game do
+  describe '#moves?' do
+    it ' should return true' do
+      players = [Player.new('Bill', 'X'), Player.new('stacy', 'O')]
+      game = Game.new(players)
+      expect(game.moves?).to eql(true)
+    end
+  end
+end
