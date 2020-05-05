e892c3b6bd8e799dc182bc13ba5b609caafdae49
Solomon APPIER-SIGN <braasig@gmail.com>
2020-05-05 19:44:06 +0000

    added test for check_win?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -112,4 +112,19 @@ describe Board do
       expect(board.check_win_rev_diagonals?('X')).to be(true)
     end
   end
+
+  describe '#check_win?' do
+    it 'should return true if the move resulted in a win' do
+      board = Board.new
+      board.move('3', 'X')
+      board.move('5', 'X')
+      board.move('7', 'X')
+      expect(board.check_win?('X')).to be(true)
+    end
+
+    it 'should return false if the move did not result in a win' do
+      board = Board.new
+      expect(board.check_win?('X')).to be(false)
+    end
+  end
 end
