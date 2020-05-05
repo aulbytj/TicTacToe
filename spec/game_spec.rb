dfd4b6538c41ab98c888a83b6b0086ea8ae0856b
Solomon APPIER-SIGN <braasig@gmail.com>
2020-05-05 20:26:41 +0000

    added tests for switch_player and player

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,16 +2,15 @@ require_relative '../lib/game'
 require_relative '../lib/player'
 
 describe Game do
+  let(:players) { %w[Bill stacy] }
+  let(:game) { Game.new(players) }
+
   describe '#moves?' do
     it ' should return true if there are moves to make on the board' do
-      players = %w[Bill stacy]
-      game = Game.new(players)
       expect(game.moves?).to eql(true)
     end
 
-    it ' should return true if there are no moves to make on the board' do
-      players = %w[Bill stacy]
-      game = Game.new(players)
+    it ' should return false if there are no moves to make on the board' do
       game.make_move('1')
       game.make_move('2')
       game.make_move('3')
@@ -22,6 +21,24 @@ describe Game do
       game.make_move('8')
       game.make_move('9')
       expect(game.moves?).to eql(false)
+    end
+  end
+
+  describe '#board' do
+    it 'should return the game board' do
+      expect(game.board).to eql("| 1 | 2 | 3 | \n| 4 | 5 | 6 | \n| 7 | 8 | 9 | \n")
+    end
+  end
+
+  describe '#switch_player' do
+    it 'should return the index of the second player: 1' do
+      expect(game.switch_player).to eql(1)
+    end
+  end
+
+  describe '#player' do
+    it 'should return the current player' do
+      expect(game.player.name).to eql(players[0])
     end
   end
 end
