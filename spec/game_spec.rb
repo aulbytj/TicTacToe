ccd6af85368dbb7df6e70ba0e0eb6115d2bda266
Solomon APPIER-SIGN <braasig@gmail.com>
2020-05-05 19:22:12 +0000

    fixed game.moves? test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,7 +4,7 @@ require_relative '../lib/player'
 describe Game do
   describe '#moves?' do
     it ' should return true' do
-      players = [Player.new('Bill', 'X'), Player.new('stacy', 'O')]
+      players = ['Bill', 'stacy']
       game = Game.new(players)
       expect(game.moves?).to eql(true)
     end
